ef4e59e1324206ce240ad773edcf0cca55ee2ee4
aistomin <andrej.istomin.ikeen@gmail.com>
2021-04-20 06:51:28 +0200

    Issue #75. Copyright is fixed.

--- src/test/java/com/github/aistomin/sexist/NameGenderTest.java
+++ src/test/java/com/github/aistomin/sexist/NameGenderTest.java
@@ -633,7 +633,7 @@
  * the "copyright" line and a pointer to where the full notice is found.
  *
  * <one line to give the program's name and a brief idea of what it does.>
- * Copyright (C) <year>  <name of author>
+ * Copyright (C) 2021  Andrej Istomin
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -653,7 +653,7 @@
  * If the program does terminal interaction, make it output a short
  * notice like this when it starts in an interactive mode:
  *
- * <program>  Copyright (C) <year>  <name of author>
+ * <program>  Copyright (C) 2021  Andrej Istomin
  * This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
  * This is free software, and you are welcome to redistribute it
  * under certain conditions; type `show c' for details.
